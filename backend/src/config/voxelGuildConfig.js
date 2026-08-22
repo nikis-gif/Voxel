aec309e4aa236a3bfcf172fd6f922c8e0b4a8876
niki <enzopereira2706@gmail.com>
2026-08-21 21:35:34 -0300

    Update voxelGuildConfig.js

--- backend/src/config/voxelGuildConfig.js
+++ backend/src/config/voxelGuildConfig.js
@@ -5,6 +5,8 @@ export const VOXEL_GUILD_CONFIG = Object.freeze({
   logChannelId: "1540406288847085648",
   trainingReportChannelId: "1540407767569928212",
   recruitmentReportChannelId: "1540407818258219088",
+  trainingAnnouncementChannelId: "1540412778332954746",
+  examAnnouncementChannelId: "1540412870314037309",
 
   botOnlyChannelIds: Object.freeze([
     "1540408247406690364",
