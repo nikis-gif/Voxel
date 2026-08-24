e694fa79553ee5e1177bd4ae8ed3ff1d87f0bd80
niki <enzopereira2706@gmail.com>
2026-08-24 18:13:32 -0300

    Update upload.js

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -11,7 +11,8 @@ export const supportUpload = multer({
   limits: {
     files: MAX_FILES,
     fileSize: MAX_FILE_SIZE,
-    fields: 4,
+    // type, sender, discordUsername, message and honeypot website
+    fields: 5,
     fieldSize: 8 * 1024
   },
   fileFilter: (_req, file, callback) => {
